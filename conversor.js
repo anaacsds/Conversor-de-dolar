88a0d2f3c421aba3cad2abb6e91e5ab3c02de612
Ana Clara <anacsds1@gmail.com>
2025-10-20 18:00:46 -0300

    Adicionando código inicial

--- conversor.js
+++ conversor.js
@@ -1,3 +1,6 @@
 function conversor() {
-
-}`
+    const valor = document.querySelector("input")
+    const dolar = 5.37;
+    const valorConvertido = valor.value / dolar //o value deixa que apenas o valor da variável "valor" seja selecionado
+    alert(`O valor em dólar é ${valorConvertido} dólares`);
+}
